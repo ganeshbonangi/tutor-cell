bb7a58703dcabaed64e0aab8e2615f970c1224ff
unknown <ganesh.bonangi@SGH049SVK5.hsmumsz.local>
2014-08-22 16:14:30 +0530

    added the sturucture

--- scripts/controllers/examSettingController.js
+++ scripts/controllers/examSettingController.js
@@ -1,3 +1,10 @@
-app.controller("examSettingController",function($scope){
+app.controller("examSettingController",function($scope,MockTestService){
 	$scope.sections=[1,2,3];
+	$scope.mockTests = MockTestService.getMocktests();
+	$scope.deleteMockTest = function(index){
+		$scope.mockTests = MockTestService.deleteMocktest(index);
+	}
+	$scope.editMockTest = function(index){
+		$scope.mockTests = MockTestService.editMocktest(index);
+	}
 });
--- scripts/services/services.js
+++ scripts/services/services.js
@@ -13,15 +13,38 @@ app.service("TutorService",function($rootScope,$sce){
 			this.addMocktest = function(){
 
 			},
-			this.deleteMocktest = function(){
-
+			this.deleteMocktest = function(index){
+				
 			},
 			this.addSections = function(){
 
 			},
-			this.editMocktest = function(){
-
-			}
+			this.editMocktest = function(index){
+				//update the json object 
+			},
+			this.getMocktests = function(){
+				return [{
+							id:1,
+							name:"sampletest",
+							sections:["section1","section2","section3","section4"],
+							className:"fifth",
+							startDate:"12/08/2014",
+							endDate:"15/09/2014",
+							time:"01:10",
+							tutorId:1,
+							theme:"default"
+						},{
+							id:2,
+							name:"sampletest2",
+							sections:["section21","section22","section23","section24"],
+							className:"sisth",
+							startDate:"12/08/2014",
+							endDate:"15/09/2014",
+							time:"02:20",
+							tutorId:1,
+							theme:"default"
+						}];
+			}	
 }).service("QuestionService",function($rootScope){
 
 			this.addQuestion = function(){
