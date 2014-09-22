c8446486bf37b3d448f6fa39c5a1c2698bc922fe
ganesh bonangi <ganesh.bonangi@gmail.com>
2014-09-22 19:01:47 +0530

    added the exam settings

--- scripts/controllers/examSettingController.js
+++ scripts/controllers/examSettingController.js
@@ -1,3 +1,98 @@
-app.controller("examSettingController",function($scope){
-	$scope.sections=[1,2,3];
+app.controller("examSettingController",function($scope,MockTestService,$modal){
+	$scope.sections=[0];
+	$scope.sectionNames=[];
+	$scope.currentMocktest={};
+	//$scope.showSettingsForm=false;
+	$scope.addMocktest = function(){
+		$scope.currentMocktest={};
+    //console.log($scope.currentMocktest);
+    $scope.open('lg');
+	}
+	$scope.editMocktest = function(index){
+		$scope.currentMocktest = $scope.mockTests[index];
+   // console.log($scope.currentMocktest);
+    $scope.open('lg');
+	}
+	$scope.deleteMocktest = function(index){
+		$scope.mockTests.splice(index,1);
+	}
+	$scope.mockTests = MockTestService.getMocktests();
+
+
+
+  $scope.open = function (size) {
+
+    var modalInstance = $modal.open({
+      templateUrl: 'myModalContent.html',
+      controller: ModalInstanceCtrl,
+      size: size,
+      resolve: {
+        currentMocktest: function () {
+          return $scope.currentMocktest;//$scope.items;
+        },
+        mockTests: function () {
+          return $scope.mockTests;
+        }
+      }
+    });
+
+    modalInstance.result.then(function (selectedItem) {
+      $scope.selected = selectedItem;
+    }, function () {
+      //$log.info('Modal dismissed at: ' + new Date());
+    });
+  };
+
+
+
 });
+
+
+var ModalInstanceCtrl = function ($scope, $modalInstance, currentMocktest, MockTestService, mockTests) {
+
+  $scope.currentMocktest = currentMocktest;
+  $scope.mockTests=mockTests;
+  if(!$scope.currentMocktest.startDate){
+    $scope.currentMocktest.startDate = new Date('2013-03-01T01:10:00');
+    //console.log($scope.currentMocktest.startDate);
+  }else{
+    $scope.currentMocktest.startDate = new Date();
+  }
+  //$scope.currentMocktest.value=new Date('2013-03-01T01:10:00');
+  $scope.selected = {
+    item:0//$scope.items[0]
+  };
+
+  $scope.save = function () {
+    MockTestService.editOrAddMocktest($scope.currentMocktest);
+    if($scope.mockTests){
+      $scope.mockTests.push($scope.currentMocktest);
+    }else{
+      $scope.mockTests=[];
+      $scope.mockTests.push($scope.currentMocktest);
+    }
+    $scope.currentMocktest={};
+    $modalInstance.close($scope.selected.item);
+  };
+
+  $scope.cancel = function () {
+    $modalInstance.dismiss('cancel');
+  };
+
+  $scope.addSection = function(){
+    if($scope.currentMocktest.sections){
+      $scope.currentMocktest.sections.push($scope.currentMocktest.sections.length);
+    }else{
+      $scope.currentMocktest.sections=[];
+      $scope.currentMocktest.sections.push($scope.currentMocktest.sections.length);
+    }
+    
+  };
+
+  $scope.deleteSection = function(){
+    try{
+      $scope.currentMocktest.sections.pop();
+    }catch(e){  }
+  };
+
+};
--- scripts/services/services.js
+++ scripts/services/services.js
@@ -1,7 +1,6 @@
 app.service("TutorService",function($rootScope,$sce){
 
 		this.addTutor = function(){
-
 			
 		},
 		this.editTutor = function(){
@@ -10,18 +9,48 @@ app.service("TutorService",function($rootScope,$sce){
 
 }).service("MockTestService",function($rootScope){
 
-			this.addMocktest = function(){
+			this.editOrAddMocktest = function(obj){
+				    if(obj.id){
+				      //hit insert service
+				    }else{
+				      //hit update service
+				    }
+				    //have to hit the service for
+				    //onsuccess{
+				    //in success have to receice the id for the current object and should ad to the current obj
+				    
+			},this.getMocktests = function(){
+				return [
+					{
+						name:"mocktest1",
+						sections:["section1","section2","section3","section4","section5"],
+						startDate:"2013-03-01T01:10:00",
+						endDate:"22-06-2014",
+						durationHH:02,
+						durationMM:22,
+						id:1
+					},{
+						name:"mocktest2",
+						sections:["section1","section2","section3","section4","section5"],
+						startDate:"22-05-2014",
+						endDate:"22-06-2014",
+						duration:"2:20",
+						id:2
+					},{
+						name:"mocktest3",
+						sections:["section1","section2","section3","section4","section5"],
+						startDate:"22-05-2014",
+						endDate:"22-06-2014",
+						duration:"3:30",
+						id:3
+					}
+				];
+			},this.deleteMocktest = function(){
 
-			},
-			this.deleteMocktest = function(){
-
-			},
-			this.addSections = function(){
-
-			},
-			this.editMocktest = function(){
+			},this.addSections = function(){
 
 			}
+
 }).service("QuestionService",function($rootScope){
 
 			this.addQuestion = function(){
